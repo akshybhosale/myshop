19ae16ec38d1f7e622f05576f45d4ef18d3524de
akshybhosale <akshaybhosale2810@gmail.com>
2023-11-23 12:26:02 +0530

    changes in depoly package.json

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -17,15 +17,15 @@ export default function Navbar(){
     </div>
     <ul className='nav-menu'>
    
-      <li onClick={()=>{setMenu('shop')}}><Link to='/' style={{textDecoration:'none'}}>Shop</Link>{menu==='shop' ? <hr/>:<></>}</li> 
-      <li onClick={()=>{setMenu('mens')}}><Link to='/mens' style={{textDecoration:'none'}}>Men</Link>{menu==='mens' ? <hr/>:<></>}</li>
-      <li onClick={()=>{setMenu('womens')}}><Link to='womens' style={{textDecoration:'none'}}>Women</Link>{menu==='womens' ? <hr/>:<></>}</li>
-      <li onClick={()=>{setMenu('kids')}}><Link to='/kids' style={{textDecoration:'none'}}>Kids</Link>{menu==='kids' ? <hr/>:<></>}</li>
+      <li onClick={()=>{setMenu('shop')}}><Link to='/myshop' style={{textDecoration:'none'}}>Shop</Link>{menu==='shop' ? <hr/>:<></>}</li> 
+      <li onClick={()=>{setMenu('mens')}}><Link to='/myshop/mens' style={{textDecoration:'none'}}>Men</Link>{menu==='mens' ? <hr/>:<></>}</li>
+      <li onClick={()=>{setMenu('womens')}}><Link to='/myshop/womens' style={{textDecoration:'none'}}>Women</Link>{menu==='womens' ? <hr/>:<></>}</li>
+      <li onClick={()=>{setMenu('kids')}}><Link to='/myshop/kids' style={{textDecoration:'none'}}>Kids</Link>{menu==='kids' ? <hr/>:<></>}</li>
     </ul>
     <div className='nav-login-cart'>
   
-       <Link to='/login'> <button>login</button></Link>
-       <Link to='/cart'><img src={cart_icon} alt='cart_icon' /></Link>
+       <Link to='/myshop/login'> <button>login</button></Link>
+       <Link to='/myshop/cart'><img src={cart_icon} alt='cart_icon' /></Link>
       <div className='nav-cart-count'>{getTotalCartItems()}</div>
     </div>
   </div>
